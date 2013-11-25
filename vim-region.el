eecdaa4c0a26c3b7d1204451a13fc3dffd56d3b1
ongaeshi <ongaeshi0621@gmail.com>
2013-11-25 21:37:25 +0900

    0.1 -> 0.2

--- vim-region.el
+++ vim-region.el
@@ -4,7 +4,7 @@
 
 ;; Author: ongaeshi <ongaeshi0621@gmail.com>
 ;; URL: https://github.com/ongaeshi/emacs-vim-region
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
